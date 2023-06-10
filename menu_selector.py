e2bdfd24f1727042c840a30cc3de8a47d371e0f5
Myeongseokkang <kms00129@naver.com>
2023-06-10 21:55:06 +0900

    [version#2] 주차장 정보로 데이터 구성, 출력, 프로그램 종료 기능 추가

--- menu_selector.py
+++ menu_selector.py
@@ -1,4 +1,10 @@
+import file_manager
+from parking_spot_manager import str_list_to_class_list, print_spots
+
 def start_process(path):
+    str_list = file_manager.read_file(path)
+    spots = str_list_to_class_list(str_list)
+
     while True:
         print("---menu---")
         print("[1] print")
@@ -7,8 +13,7 @@ def start_process(path):
         print("[4] exit")
         select = int(input('type:'))
         if select == 1:
-            print("not implemented yet")
-            # fill this block
+            print_spots(spots)
         elif select == 2:
             print("---filter by---")
             print("[1] name")
@@ -52,7 +57,7 @@ def start_process(path):
                 # fill this block
             else: print("invalid input")
         elif select == 4:
-            print("not implemented yet")
-            # fill this block
+            print("Exit")
+            break
         else:
             print("invalid input")
--- parking_spot_manager.py
+++ parking_spot_manager.py
@@ -1,21 +1,81 @@
 class parking_spot:
-    # you have to implement 'constructor(생성자)' and 'get' method
-    def __str__(self):
+    """
+    하나의 주차장 정보를 저장하는 클래스
+
+    Attributes:
+        name (str): 주차장 이름
+        city (str): 주차장이 위치한 시
+        district (str): 주차장이 위치한 구
+        ptype (str): 주차장 유형
+        longitude (float): 경도
+        latitude (float): 위도
+
+    methods:
+        get(keyword): keyword에 해당하는 정보를 반환
+    """
+
+    # 생성자
+    def __init__(self, name, city, district, ptype, longitude, latitude):
+        self.__item = {}
+        self.__item['name'] = name
+        self.__item['city'] = city
+        self.__item['district'] = district
+        self.__item['ptype'] = ptype
+        self.__item['longitude'] = float(longitude)
+        self.__item['latitude'] = float(latitude)
+
+    def get(self, keyword='name') -> dict:
+        return self.__item[keyword]
+
+
+    def __str__(self) -> str:
         item = self.__item
         s  = f"[{item['name']}({item['ptype']})] "
         s += f"{item['city']} {item['district']}"
         s += f"(lat:{item['latitude']}, long:{item['longitude']})"
         return s
+    
+def str_list_to_class_list(str_list: list) -> list:
+    """
+    문자열 리스트로 저장된 주차장 정보를 클래스 리스트로 변환
+
+    Args:
+        str_list (list): 문자열 리스트
+
+    Returns:
+        list: 클래스 리스트
+    """
+    class_list = []
+    for item in str_list:
+        # 1. 아이템 파싱(delimiter : ',')
+        seq,name,city,district,ptype,longitude,latitude = item.split(',')
+        # 2. 파싱 정보로 클래스 생성
+        new_item = parking_spot(name,city,district,ptype,longitude,latitude)
+        # 3. class_list에 추가
+        class_list.append(new_item)
+
+    return class_list
+
+def print_spots(spots: list) -> None:
+    """
+    주차장 정보를 출력
+
+    Args:
+        spots (list): 주차장 정보가 저장된 클래스 리스트
+    """
+    print(f"---print elements({len(spots)})---")
+    for item in spots:
+        print(item)
 
 
 # 각 단계별로 테스트 (테스트할때 주석해제 후 사용)
 if __name__ == '__main__':
     print("Testing the module...")
     # version#2
-    # import file_manager
-    # str_list = file_manager.from_file("./input/free_parking_spot_seoul.csv")
-    # spots = str_list_to_class_list(str_list)
-    # print_spots(spots)
+    import file_manager
+    str_list = file_manager.read_file("./input/free_parking_spot_seoul.csv")
+    spots = str_list_to_class_list(str_list)
+    print_spots(spots)
 
     # version#3
     # spots = filter_by_district(spots, '동작')
